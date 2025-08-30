3240de48a0351ece25a02a897bbda4c4ef02f76c
Greg Jones <gregj@thesoftwarecottage.com.au>
2025-08-30 15:10:06 +1000

    End of unit 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dapper-torrone-7e13dc.netlify.app/'
+  site: 'https://dapper-torrone-7e13dc.netlify.app/',
+  integrations: [preact()]
 });
